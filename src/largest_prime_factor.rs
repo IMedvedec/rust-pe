9f0cc636ee93935bbefd7adff1aa3a78df053c61
imedvedec <ivan.medvedec0@gmail.com>
2021-08-10 21:31:51 +0200

    PE: largest_prime_factor addition bugfix and implemented division

--- src/largest_prime_factor.rs
+++ src/largest_prime_factor.rs
@@ -4,9 +4,9 @@ pub fn largest_prime_factor(x: &str) -> String {
         .rev()
         .map(|c| c.to_digit(10).unwrap() as u8)
         .collect();
-    let sub: &mut [u8; 3] = &mut [0, 1, 0];
+    let sub: &mut [u8; 3] = &mut [2, 0, 0];
 
-    let x = addition(number.as_mut_slice(), sub).expect("None returned on subtraction");
+    let x = division(number.as_mut_slice(), sub).expect("None returned on subtraction");
     println!("{:?}", x);
 
     String::from_utf8_lossy(&x).to_string()
@@ -38,11 +38,11 @@ fn addition<'a>(first_addend: &'a mut [u8], second_addend: &mut [u8]) -> Option<
     let mut carry = 0_u8;
 
     for (fa, sa) in first_addend.iter_mut().zip(second_addend) {
-        if (*fa + *sa) >= 10 {
-            *fa = (*fa + *sa) - 10;
+        if (*fa + *sa + carry) >= 10 {
+            *fa = (*fa + *sa + carry) - 10;
             carry = 1_u8;
         } else {
-            *fa = *fa + *sa;
+            *fa = *fa + *sa + carry;
             carry = 0_u8;
         }
     }
@@ -52,4 +52,25 @@ fn addition<'a>(first_addend: &'a mut [u8], second_addend: &mut [u8]) -> Option<
     }
 
     Some(first_addend)
+}
+
+fn division(dividend: &mut [u8], divisor: &mut [u8]) -> Option<Vec<u8>> {
+    let mut result: Vec<u8> = vec![0; dividend.len()];
+    let mut unit: Vec<u8> = vec![0; dividend.len()];
+    unit[0] = 1;
+
+    loop {
+        match subtract(dividend, divisor) {
+            None => return None,
+            Some(x) => {
+                result = addition(result.as_mut_slice(), unit.as_mut_slice())
+                    .unwrap()
+                    .to_vec();
+
+                if x.iter().fold(true, |acc, elem| acc && !(*elem > 0)) {
+                    return Some(result);
+                }
+            }
+        }
+    }
 }
