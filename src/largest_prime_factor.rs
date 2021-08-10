ad05ca8b3ae8d56bb78f317d639f107bd4bb9754
imedvedec <ivan.medvedec0@gmail.com>
2021-08-10 21:16:30 +0200

    PE: largest_prime_factor refactor subtract to option result

--- src/largest_prime_factor.rs
+++ src/largest_prime_factor.rs
@@ -4,16 +4,17 @@ pub fn largest_prime_factor(x: &str) -> String {
         .rev()
         .map(|c| c.to_digit(10).unwrap() as u8)
         .collect();
-    let sub: &mut [u8; 6] = &mut [3, 2, 1, 0, 0, 0];
+    let sub: &mut [u8; 3] = &mut [0, 1, 0];
 
-    let x = subtract(number.as_mut_slice(), sub);
+    let x = subtract(number.as_mut_slice(), sub).expect("None returned on subtraction");
+    println!("{:?}", x);
 
     String::from_utf8_lossy(&x).to_string()
 }
 
 /// Large number subtraction represented by u8 slices.
 /// Slice parameters are reversed in order because of the subtraction algorithm.
-fn subtract<'a>(minuend: &'a mut [u8], subtrahend: &mut [u8]) -> &'a [u8] {
+fn subtract<'a>(minuend: &'a mut [u8], subtrahend: &mut [u8]) -> Option<&'a [u8]> {
     let mut carry = 0_u8;
 
     for (m, s) in minuend.iter_mut().zip(subtrahend) {
@@ -27,10 +28,8 @@ fn subtract<'a>(minuend: &'a mut [u8], subtrahend: &mut [u8]) -> &'a [u8] {
     }
 
     if carry > 0 {
-        for m in minuend.iter_mut() {
-            *m = 0
-        }
+        return None;
     }
 
-    minuend
+    Some(minuend)
 }
