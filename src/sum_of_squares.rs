e50e488ccf63b76d1d0cf769dd959659b60a278e
imedvedec <ivan.medvedec0@gmail.com>
2021-08-13 21:00:46 +0200

    PE: sum_of_squares initial parallel solution

--- src/sum_of_squares.rs
+++ src/sum_of_squares.rs
@@ -7,6 +7,60 @@ pub fn sum_of_squares(val: u64) -> u64 {
         .sum()
 }
 
+pub fn sum_of_squares_parallel(val: u64) -> u64 {
+    use std::sync::mpsc;
+    use std::thread;
+
+    let interval = val / 16;
+
+    let seq1 = 1..(8 * interval);
+    let seq2 = (8 * interval)..(8 * interval + 4 * interval);
+    let seq3 = (12 * interval)..(12 * interval + 3 * interval);
+    let seq4 = (15 * interval)..=val;
+
+    let (tx1, rx1): (mpsc::Sender<u64>, mpsc::Receiver<u64>) = mpsc::channel();
+    thread::spawn(move || {
+        tx1.send(
+            seq1.into_iter()
+                .map(|x| largest_perfect_square_division(x))
+                .sum(),
+        )
+        .unwrap()
+    });
+
+    let (tx2, rx2): (mpsc::Sender<u64>, mpsc::Receiver<u64>) = mpsc::channel();
+    thread::spawn(move || {
+        tx2.send(
+            seq2.into_iter()
+                .map(|x| largest_perfect_square_division(x))
+                .sum(),
+        )
+        .unwrap()
+    });
+
+    let (tx3, rx3): (mpsc::Sender<u64>, mpsc::Receiver<u64>) = mpsc::channel();
+    thread::spawn(move || {
+        tx3.send(
+            seq3.into_iter()
+                .map(|x| largest_perfect_square_division(x))
+                .sum(),
+        )
+        .unwrap()
+    });
+
+    let (tx4, rx4): (mpsc::Sender<u64>, mpsc::Receiver<u64>) = mpsc::channel();
+    thread::spawn(move || {
+        tx4.send(
+            seq4.into_iter()
+                .map(|x| largest_perfect_square_division(x))
+                .sum(),
+        )
+        .unwrap()
+    });
+
+    rx1.recv().unwrap() + rx2.recv().unwrap() + rx3.recv().unwrap() + rx4.recv().unwrap()
+}
+
 fn largest_perfect_square_division(val: u64) -> u64 {
     let mut perfect_square_iter = PerfectSquareIter::new();
 
