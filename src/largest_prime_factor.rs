f47e8c5950b20182d1c7700ac67929ee6e5046f4
imedvedec <ivan.medvedec0@gmail.com>
2021-08-13 16:04:49 +0200

    PE: largest_prime_factor solution with u64 primitive type

--- src/largest_prime_factor.rs
+++ src/largest_prime_factor.rs
@@ -1,3 +1,38 @@
 pub fn largest_prime_factor(val: u64) -> u64 {
-    panic!("not implemented")
+    use std::collections::btree_map::BTreeMap;
+    let mut prime_factors: BTreeMap<u64, u64> = BTreeMap::new();
+
+    let mut mut_val = val;
+    let mut prime_iterator = 2_u64;
+    while prime_iterator <= mut_val {
+        if !is_prime(prime_iterator) {
+            prime_iterator += 1;
+            continue;
+        }
+
+        if mut_val % prime_iterator == 0 {
+            let prime_counter = prime_factors.entry(prime_iterator).or_insert(0);
+            mut_val /= prime_iterator;
+            *prime_counter += 1;
+        } else {
+            prime_iterator += 1;
+        }
+    }
+
+    *prime_factors
+        .keys()
+        .last()
+        .expect(format!("Largest prime not found for number: {}", val).as_str())
+}
+
+fn is_prime(val: u64) -> bool {
+    let upper_bound = (val as f64).sqrt().ceil() as u64;
+
+    for x in 2..=upper_bound {
+        if val % x == 0 {
+            return false;
+        }
+    }
+
+    true
 }
